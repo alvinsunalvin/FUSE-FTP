5628b3eeb9c92a6e08551d01260d457cd25e274a
Yuanhang Zheng <zyh971025@126.com>
2018-06-12 22:04:15 +0800

    fix scripts

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ mkdir build
 cd build
 ../meson/meson.py ..
 ninja
+sudo cp util/fusermount3 /usr/bin/fusermount3
+sudo chown root:root /usr/bin/fusermount3
+sudo chmod 4755 /usr/bin/fusermount3
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd build/example >/dev/null 2>&1
 sudo rm -rf /mnt/fuse >/dev/null 2>&1
 sudo mkdir /mnt/fuse >/dev/null 2>&1
 sudo chmod 777 /mnt/fuse >/dev/null 2>&1
-sudo ./fuse-ftp /mnt/fuse -d
+./fuse-ftp /mnt/fuse -d
 sudo umount /mnt/fuse >/dev/null 2>&1
